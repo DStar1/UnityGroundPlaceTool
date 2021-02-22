7a493beb3efbba58c179a4a7b32ff7a0d17c286d
dstar1 <dreamstarind@gmail.com>
2021-02-21 21:08:10 -0800

    first commit

---
+++ GroundPlacementController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using UnityEngine;
+
+public class GroundPlacementController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] placeableObjectPrefabs;
+
+    private GameObject currentPlaceableObject;
+
+    private float mouseWheelRotation;
+    private int currentPrefabIndex = -1;
+
+    private bool flag = false;
+    private bool rotScaleBool = false;
+    private float scale = 13f;
+
+    private void Update()
+    {
+        HandleNewObjectHotkey();
+
+        if (currentPlaceableObject != null)
+        {
+            MoveCurrentObjectToMouse();
+            flag = PrintIfMiddleClicked();
+            if (flag)
+            {
+                rotScaleBool = rotScaleBool ? false : true;
+                flag = false;
+            }
+            RotateScaleFromMouseWheel(rotScaleBool);
+            ReleaseIfClicked();
+        }
+    }
+
+    private void HandleNewObjectHotkey()
+    {
+        for (int i = 0; i < placeableObjectPrefabs.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0 + 1 + i))
+            {
+                if (PressedKeyOfCurrentPrefab(i))
+                {
+                    Destroy(currentPlaceableObject);
+                    currentPrefabIndex = -1;
+                }
+                else
+                {
+                    if (currentPlaceableObject != null)
+                    {
+                        Destroy(currentPlaceableObject);
+                    }
+
+                    currentPlaceableObject = Instantiate(placeableObjectPrefabs[i]);
+                    currentPrefabIndex = i;
+                }
+
+                break;
+            }
+        }
+    }
+
+    private bool PressedKeyOfCurrentPrefab(int i)
+    {
+        return currentPlaceableObject != null && currentPrefabIndex == i;
+    }
+
+    private void MoveCurrentObjectToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            currentPlaceableObject.transform.position = hitInfo.point;
+            currentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            currentPlaceableObject.transform.localScale = new Vector3(scale, scale, scale);
+        }
+    }
+
+    private void RotateScaleFromMouseWheel(bool rotScaleBool)
+    {
+        Debug.Log(Input.mouseScrollDelta);
+        if (rotScaleBool)
+        {
+            scale += Input.mouseScrollDelta.y;
+            currentPlaceableObject.transform.localScale = new Vector3(scale, scale, scale);
+        }
+        else
+        {
+            mouseWheelRotation += Input.mouseScrollDelta.y;
+            currentPlaceableObject.transform.Rotate(Vector3.up, mouseWheelRotation * 10f);
+        }
+    }
+
+    private void ReleaseIfClicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentPlaceableObject = null;
+        }
+    }
+
+    private bool PrintIfMiddleClicked()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            Debug.Log("Testing Middle Click!");
+            return true;
+        }
+        return false;
+    }
+}
